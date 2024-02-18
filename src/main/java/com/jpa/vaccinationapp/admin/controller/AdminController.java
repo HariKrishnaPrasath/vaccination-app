cbd42c86e45854c39a797e735cde097246060c68
senthuranravi-cse <senthuran.ravi02@gmail.com>
2024-02-18 23:25:29 +0530

    First Commit

--- src/main/java/com/jpa/vaccinationapp/admin/controller/AdminController.java
+++ src/main/java/com/jpa/vaccinationapp/admin/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-//by Senthuran
+
 @RestController
 public class AdminController {
     @Autowired
