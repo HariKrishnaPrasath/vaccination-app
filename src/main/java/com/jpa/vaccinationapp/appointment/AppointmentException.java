798d288938476aa2e951133312a12402a824ecfa
S <HS22@ford.com>
2024-02-21 22:31:03 +0530

    Checking admin

---
+++ src/main/java/com/jpa/vaccinationapp/appointment/AppointmentException.java
@@ -0,0 +1,7 @@
+package com.jpa.vaccinationapp.appointment;
+
+public class AppointmentException extends Exception{
+    public AppointmentException(String message) {
+        super(message);
+    }
+}
