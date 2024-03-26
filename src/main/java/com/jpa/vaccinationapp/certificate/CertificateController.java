d1ba206d8bee87b0185d79c3c32a7148a71bfaf7
S <HS22@ford.com>
2024-03-26 20:38:23 +0530

    change done for react js

--- src/main/java/com/jpa/vaccinationapp/certificate/CertificateController.java
+++ src/main/java/com/jpa/vaccinationapp/certificate/CertificateController.java
@@ -16,7 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"})
 
 public class CertificateController {
     @Autowired
